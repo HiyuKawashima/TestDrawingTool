ae4bd15e4f6f04139d9fe6323fd752ee01bc8f09
川島 陽悠 <s01721@CG3071.local>
2018-04-16 16:25:58 +0900

    second commit

--- TestDrawingTool.java
+++ TestDrawingTool.java
@@ -2,6 +2,8 @@ public class TestDrawingTool extend TestCase(){
     public void testDrawAllShapes(){
         Circle circle = new BigCircle(10, new Point(20,20));
         Square square = new BigSquare(50, new Point(10,10));
+        circle.setPriority(0);
+        square.setPriority(1);
         DrawingTool drawingTool = new DrawingTool();
         List<Shape> shapeList = new ArrayList<Shape>();
         shapeList.add(circle);
@@ -10,43 +12,98 @@ public class TestDrawingTool extend TestCase(){
     }
 }
 
+interface Shape {
+    
+}
+
 public class Point(int p1, int p2){
     int x = p1;
     int y = p2;
 }
 
-public class Circle(){
+public abstract class Circle implements Shape{
     double radius;
     Point point;
+    int priority;
+
+    abstract void Draw();
+    abstract boolean Priority(int i);
+    abstract setPriority(int p);
 }
 
-public class BigCircle extend Circle(double r, Point p){
+public class BigCircle(double r, Point p) extend Circle{
     radius = r;
     point = p;
 
+    @override
     void Draw(){
         //rとpを使っていい感じに円を描画
     }
+
+    @override
+    boolean Priority(int i){
+        if(priority == i){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    @override
+    public void setPriority(int p){
+        this.priority = p;
+    }
 }
 
-public class Square(){
+public abstract class Square implements Shape{
     int wide;
     Point point;
+    int priority;
+
+    abstract void Draw();
+    abstract boolean Priorityint(int i);
+    abstract setPriority(int p);
+
 }
 
-public class BigSquare extend Square(int w, Point p){
+public class BigSquare(int w, Point p) extend Square{
     wide = w;
     point = p;
 
+    @override
     void Draw(){
         //rとpを使っていい感じに四角を描画
+    }
+
+    @override
+    boolean Priority(int i){
+        if(priority == i){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    @override
+    public void setPriority(int p){
+        this.priority = p;
     }
 }
 
 public class drawingTool(){
-    public void DrawAllShapes(ArrayList<Shape> arr){
-        for(int i = 0; i < arr.size; i++){
-            arr[i].Draw();
+    public void DrawAllShapes(List<Shape> shapeList){
+        int count = 0;
+        int size = shapeList.size;
+        int i = 0;
+
+        while(count <= size){
+            for(Shape shape : shapeList){
+                if(shape.priority(i)){
+                    shape.Draw();
+                    count++;
+                }
+            }
+            i++;
         }
     }
 }
